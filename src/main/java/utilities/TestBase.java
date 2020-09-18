843f8d003bb7ae3db3ea6c700153ac163489a022
Bakry13 <mohamed.bakry13@gmail.com>
2020-09-18 19:28:51 +0200

    running firefox

--- src/main/java/utilities/TestBase.java
+++ src/main/java/utilities/TestBase.java
@@ -1,16 +1,10 @@
 package utilities;
 
-import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Optional;
@@ -35,31 +29,12 @@ public class TestBase extends AbstractTestNGCucumberTests
 			}
 			else if(Browser.equalsIgnoreCase("Firefox"))
 			{
-				System.setProperty("WebDriver.gecko.driver", driverPath+"geckodriver.exe");
-				File pathBinary = new File("C:\\Users\\V19MOsman\\AppData\\Local\\Mozilla Firefox\\firefox.exe");
-				FirefoxBinary firefoxBinary = new FirefoxBinary(pathBinary);   
-				DesiredCapabilities desired = DesiredCapabilities.firefox();
-				FirefoxOptions options = new FirefoxOptions();
-				desired.setCapability(FirefoxOptions.FIREFOX_OPTIONS, options.setBinary(firefoxBinary));
-				driver = new FirefoxDriver(options);
-			}
-			else if(Browser.equalsIgnoreCase("IE"))
-			{
-				DesiredCapabilities ieCapabilities = DesiredCapabilities.internetExplorer();
-				ieCapabilities.setCapability("EnableNativeEvents", true);    
-				ieCapabilities.setCapability("unexpectedAlertBehaviour", "accept");
-				ieCapabilities.setCapability("ignoreProtectedModeSettings", true);
-				ieCapabilities.setCapability("disable-popup-blocking", true);
-				ieCapabilities.setCapability("enablePersistentHover", true);
-				ieCapabilities.setCapability("ignoreZoomSetting", true);
-				ieCapabilities.setCapability("RequireWindowFocus", true);
-				
-				System.setProperty("webdriver.ie.driver", driverPath+"IEdriverServer.exe");
-				driver = new InternetExplorerDriver(ieCapabilities);
+				System.setProperty("webdriver.gecko.driver", driverPath+"geckodriver.exe");
+				driver = new FirefoxDriver();
 			}
 			driver.manage().deleteAllCookies();
 			driver.manage().window().maximize();
-			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); //wait until loading the initial webpage
+			driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS); //wait until loading the initial webpage
 			driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 		} catch (Exception e) {
 			e.printStackTrace();
